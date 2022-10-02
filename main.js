989672b6db181c1ae348fe7a8706ff25cf9ccf0e
Jean Senior <jeansenior212@gmail.com>
2022-10-02 12:36:21 +0100

    Added new files
    Project1_expressions_alert

---
+++ main.js
@@ -0,0 +1,4 @@
+let x = 24;   // assign the value 24 to x
+let y = 32;   // assign the value 32 to y
+let z = x + y;   // assign the value 56 to z (24 + 32)
+document.getElementById("example").innerHTML = z;
